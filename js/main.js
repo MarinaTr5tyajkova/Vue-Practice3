347dfeef742f4bb14fb08a6d78c4165aade1141c
MarinaTr5tyakova <tretyakowamarina26@gmail.com>
2025-03-21 01:39:16 +0700

    creating columns

--- js/main.js
+++ js/main.js
@@ -1,8 +1,34 @@
 Vue.component('kanban-board', {
+    template: `
+        <div class="kanban-board">
+            <kanban-column 
+                v-for="(column, index) in columns" 
+                :key="index" 
+                :title="column.title"
+            ></kanban-column>
+        </div>
+    `,
+    data() {
+        return {
+            columns: [
+                { title: "Запланированные задачи" },
+                { title: "Задачи в работе" },
+                { title: "Тестирование" },
+                { title: "Выполненные задачи" }
+            ]
+        };
+    }
 });
 
 Vue.component('kanban-column', {
-
+    props: {
+        title: String
+    },
+    template: `
+        <div class="column">
+            <h2>{{ title }}</h2>
+        </div>
+    `
 });
 
 Vue.component('kanban-card', {
