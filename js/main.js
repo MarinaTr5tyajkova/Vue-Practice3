991c6b0ed8ee2fe83608e7cdee9116b9a90a310b
MarinaTr5tyakova <tretyakowamarina26@gmail.com>
2025-03-21 01:22:25 +0700

    outline of the structure

---
+++ js/main.js
@@ -0,0 +1,18 @@
+Vue.component('kanban-board', {
+});
+
+Vue.component('kanban-column', {
+
+});
+
+Vue.component('kanban-card', {
+
+});
+
+Vue.component('kanban-modal', {
+   
+});
+
+let app = new Vue({
+    el: '#app'
+});
